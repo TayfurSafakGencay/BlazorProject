23e5fa0603f36fb0406195081dfe01c4a6e620a5
TayfurSafakGencay <safak.gencay@gmail.com>
2024-05-04 10:17:07 +0300

    Intial Commit

---
+++ MealOrder/Client/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Blazored.Modal;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MealOrder.Client
+{
+  public class Program
+  {
+    public static async Task Main(string[] args)
+    {
+      WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<App>("#app");
+
+      builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+      builder.Services.AddBlazoredModal();
+
+      await builder.Build().RunAsync();
+    }
+  }
+}
